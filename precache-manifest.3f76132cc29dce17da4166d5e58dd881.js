b2a0270e15ab6c4df155466ad3243a091f2b9703
Trang Le <trangle1405@gmail.com>
2019-07-24 16:20:40 +0700

    Updates

---
+++ precache-manifest.3f76132cc29dce17da4166d5e58dd881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197a238b47ede921be071637ace4ccb0",
+    "url": "/Todo-List-byReact/index.html"
+  },
+  {
+    "revision": "1743037d82b7869f6d8d",
+    "url": "/Todo-List-byReact/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "af2b0d601ce972a86b0e",
+    "url": "/Todo-List-byReact/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1743037d82b7869f6d8d",
+    "url": "/Todo-List-byReact/static/js/2.21307c79.chunk.js"
+  },
+  {
+    "revision": "af2b0d601ce972a86b0e",
+    "url": "/Todo-List-byReact/static/js/main.7a3de0a8.chunk.js"
+  },
+  {
+    "revision": "bf4a78dc8fbf8a22242c",
+    "url": "/Todo-List-byReact/static/js/runtime~main.3a26f2db.js"
+  }
+]);
